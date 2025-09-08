9d8325f745e3545007691bb41739aa5084b6c261
jnunez54 <jaime54a@gmail.com>
2025-09-08 16:10:01 -0600

    multi objective update

--- src/search_algorithms/multi_objective.py
+++ src/search_algorithms/multi_objective.py
@@ -1,19 +1,28 @@
-import numpy as np
-import matplotlib.pyplot as plt
+# Implementación de algoritmos de búsqueda multiobjetivo
+# Implementa NSGA II, con los objetivos: Predcción del modelo sustituto (1-IoU) y número de parámetros del modelo
+# To Do:
+# - Generalizar la selección Mu + Lambda
+# - Implementar otros algoritmos poblacionalesb (Genético primero)
+# - Comparación con PYMOO
+
+import random
+import sys
 from typing import Literal, List
+
+import matplotlib.pyplot as plt
+import numpy as np
+
 from codec import Chromosome
 from .surrogate import SurrogateModel 
-import random
-#from .surrogate import SurrogateModel
- 
-import numpy as np
-import matplotlib.pyplot as plt
-from typing import Literal, List
 
 class Evaluator():
   def __init__(self, codification: Literal["binary", "real"]) -> None:
     """
-    Clase para evaluar la población de individuos.
+    Clase del evaluador multiobjetivo.
+    
+    Parametros:
+    ===========
+        - codifiication (str): Tipo de codificación, puede ser "binary" o "real".
     """
     self.surrogate_model = SurrogateModel(model_path = r"./sustituto/xgboost_model.json")
     self.codification = codification
@@ -21,34 +30,43 @@ class Evaluator():
   def get_params(self, population: np.ndarray) -> np.ndarray:
     """
     Obtener los parámetros de la población de individuos.
-    Args:
-        population (np.ndarray): Población de individuos a evaluar.
-    Returns:
-        np.ndarray: Parámetros de los individuos.
+    
+    Parametros:
+    ===========
+        - population (np.ndarray): Población de individuos a evaluar (Arreglo de cromosomas).
+    Retorna:
+    ===========
+        - np.ndarray: Arreglo con los parámetros de los individuos.
     """
+    # Inicializar el array
     params = np.zeros((len(population), 1))
+    
     for i in range(len(population)):
         model = Chromosome(chromosome=list(population[i]))
         unet = model.get_unet()
+        # Contar parámetros entrenables
         trainable_params = sum(p.numel() for p in unet.parameters() if p.requires_grad)
         params[i] = trainable_params
     return params
 
   def evaluate_population(self, population):
     """
-    Evaluar la población de individuos.
-    Args:
-        population (np.ndarray): Población de individuos a evaluar.
-    Returns:
-        np.ndarray: Aptitudes de los individuos.
+    Obtiene el fitness de la población de individuos.
+    
+    Parametros:
+    ===========
+        - population (np.ndarray): Población de individuos a evaluar (Arreglo de cromosomas).
+    Retorna:
+    ===========
+        - np.ndarray: Aptitudes de los individuos.
     """
-    # Primer objetivo (Predicción de IoU)
+    # Evaluación del primer objetivo (Predicción de IoU)
     fitness_1 = self.surrogate_model.predict(population)
     fitness_1[fitness_1 < 0] = - np.inf
     fitness_1[fitness_1 > 1] = - np.inf
-    fitness_1 = 1 - fitness_1  # Invertir la función objetivo para minimizar
-    
-    # Segundo objetivo (Número de parámetros)
+    fitness_1 = 1 - fitness_1  # Se invierte la función objetivo (Para minimizar)
+
+    # Evaluación del segundo objetivo (Número de parámetros)
     fitness_2 = self.get_params(population)
     
     # Matriz de fitness
@@ -59,7 +77,8 @@ class Evaluator():
 class TwoObjectiveOptimizer:
     def __init__(self) -> None:
         """
-        Clase que generaliza la optimización multiobjetivo y sus operadores.
+        Clase base para optimizadores multiobjetivo con dos objetivos.
+        Implementa métodos comunes para los algorítmos poblacionales.
         """
         self.evaluator = Evaluator(codification="real")
         self.population = []
@@ -68,36 +87,56 @@ class TwoObjectiveOptimizer:
         self.pareto_fronts = []
         self.pareto_history = []
 
-    def initialize_population(self, n_pop: int) -> np.ndarray:
+    def initialize_population(self, n_pop: int) -> None:
         """
         Inicializar la población de individuos.
-        Args:
-            n_pop (int): Número de individuos en la población.
+        
+        Parametros:
+        ===========
+            - n_pop (int): Número de individuos en la población.
         """
         self.population = [Chromosome() for _ in range(n_pop)]
         self.population = np.array([chromosome.get_real() for chromosome in self.population])
         
     def get_pareto_front(self, fitness: np.ndarray) -> np.ndarray:
         """
-        Obtiene el frente de Pareto de la población dada para dos objetivos.
-        Devuelve índices de la población que forman el frente de Pareto.
+        Obtiene el frente de Pareto para dos objetivos dada una matriz de fitness.
+        Parametros:
+        ===========
+            - fitness (np.ndarray): Matriz de fitness de la población (cada fila es un individuo).
+        Retorna:
+        ===========
+            - np.ndarray: Índices de la población que forman el frente de Pareto.
         """
+        ### Métod de selección para dos objetivos
+        ### Se ordenan según el primer objetivo, así se asegura que el primer objetivo siempre mejora
+        ### Para que un punto sea no dominado, el segundo objetivo debe mejorar respecto al mínimo actual
+        ### Si no mejora, será dominado por un punto anterior (mejor en el primer objetivo y mejor o igual en el segundo)
+        
+        # Ordenar por el primer objetivo
         sorted_idxs = np.argsort(fitness[:, 0])
         sorted_fitness = fitness[sorted_idxs]
 
+        # Seleccionar puntos no dominados
         pareto_front_sorted = []
-        obj2_min = np.inf
+        obj2_min = np.inf # Valor inicial alto para el segundo objetivo
         for i in range(len(sorted_fitness)):
+            # Condición de no dominancia (Si el segundo objetivo es menor que el mínimo actual)
             if sorted_fitness[i, 1] < obj2_min:
                 pareto_front_sorted.append(sorted_idxs[i])
                 obj2_min = sorted_fitness[i, 1]
-
         return np.array(pareto_front_sorted)
 
     def ranked_pareto_fronts(self, fitness: np.ndarray) -> List[np.ndarray]:
         """
-        Obtiene los frentes de Pareto ordenados por dominancia (índices globales).
-        Devuelve una lista de arrays, cada uno representa los índices de un frente de pareto
+        Obtiene los frentes de Pareto para una matriz de fitness.
+        
+        Parametros:
+        ===========
+            - fitness (np.ndarray): Matriz de fitness de la población (cada fila es un individuo).
+        Retorna:
+        ===========
+            - List[np.ndarray]: Lista de frentes de Pareto, cada uno es un array de índicess de la población.
         """
         fronts = []
         remaining = np.arange(len(fitness))
@@ -113,18 +152,23 @@ class TwoObjectiveOptimizer:
     
     def get_crowding_distance(self, front: np.ndarray, fitness: np.ndarray) -> np.ndarray:
         """
-        Calcula la distancia de hacinamiento para un frente de Pareto dado.
-        Recibe los índices del frente y la matriz de fitness
+        Calcula la distancia de ahincamiento para un frente de Pareto dado.
         
-        Devuelve un array de crowding distances para los individuos en el frente.
+        Parametros:
+        ===========
+            - front (np.ndarray): Índices de los individuos en el frente de Pareto.
+            - fitness (np.ndarray): Matriz de fitness de la población (cada fila es un individuo).
+            
+        Retorna:
+        ===========
+            - np.ndarray: Distancias de ahincamiento para los individuos en el frente.
         """
         if len(front) == 0:
             return np.array([])
 
         distances = np.zeros(len(front))
-        num_objectives = fitness.shape[1]
 
-        for i in range(num_objectives):
+        for i in range(2): # Para cada objetivo
             # Ordenar el frente por el objetivo i
             sorted_indices = np.argsort(fitness[front, i])
             sorted_fitness = fitness[front][sorted_indices, i]
@@ -140,6 +184,7 @@ class TwoObjectiveOptimizer:
             # Calcular crowding para los puntos intermedios
             for j in range(1, len(front) - 1):
                 if not np.isinf(distances[sorted_indices[j]]):  # No sobrescribir extremos
+                    # Sumar la distancia normalizada en el objetivo i
                     distances[sorted_indices[j]] += (
                         (sorted_fitness[j + 1] - sorted_fitness[j - 1]) / (f_max - f_min)
                     )
@@ -164,12 +209,12 @@ class DiferentialEvolution(TwoObjectiveOptimizer):
     def __init__(self,  base: Literal["random", "current"] = "random", 
                 n_differences: int = 1, crossover: Literal["bin", "exp"] = "bin") -> None:
         """
-        Clase para el algoritmo de evolución diferencial.
+        Clase para el algoritmo de evolución diferencial (Adaptación para dos objetivos).
 
-        Args:
-            base (Literal["random", "current"]): Estrategia base para la evolución diferencial.
-            n_differences (int): Número de vectores diferencia a considerar, puede ser 1 o 2.
-            crossover (Literal["bin", "exp"]): Tipo de cruce a utilizar, puede ser binario o exponencial.
+        Parametros:
+            - base (str): Estrategia base para la evolución diferencial. Puede ser "random" o "current".
+            - n_differences (int): Número de vectores diferencia a considerar, puede ser 1 o 2.
+            - crossover (str): Tipo de cruce a utilizar, puede ser "bin" o "exp".
         """
         super().__init__()
         assert base in ["random", "current"], "base must be 'random' or 'current'"
@@ -186,11 +231,12 @@ class DiferentialEvolution(TwoObjectiveOptimizer):
         """
         Iniciar el algoritmo de evolución diferencial.
 
-        Args:
-            n_pop (int): Número de individuos en la población.
-            max_gen (int): Número máximo de generaciones.
-            mutation_rate (float): Tasa de mutación.
-            crossover_rate (float): Tasa de cruce.
+        Parametros:
+        ===========
+            - n_pop (int): Número de individuos en la población.
+            - max_gen (int): Número máximo de generaciones.
+            - mutation_rate (float): Tasa de mutación.
+            - crossover_rate (float): Tasa de cruce.
         """
         self.initialize_population(n_pop)
         for gen in range(max_gen):
@@ -198,80 +244,65 @@ class DiferentialEvolution(TwoObjectiveOptimizer):
             offspring = []
             for target in range(len(self.population)):
                 # Selección del vector base
-                if self.variant_dict["base"] == "random":
+                if self.variant_dict["base"] == "random": # random to best
                     r1 = np.random.randint(0, len(self.population))
-                else: # current
+                else: # current to best
                     r1 = target
+
                 # Selección de los vectores diferencia
                 r2, r3 = random.sample([j for j in range(len(self.population)) if j != target and j != r1], 2)
-                if self.variant_dict["n_differences"] == 1:
+                if self.variant_dict["n_differences"] == 1: # Caso de un vector diferencia
                     trial = self.population[r1] + mutation_rate * (self.population[r2] - self.population[r3])
-                elif self.variant_dict["n_differences"] == 2:
+                else: # Caso de dos vectores diferencia
                     r4, r5 = random.sample([j for j in range(len(self.population)) if j != target and j != r1 and j != r2 and j != r3], 2)
                     trial = self.population[r1] + mutation_rate * (
                             (self.population[r2] - self.population[r3]) + 
                             (self.population[r4] - self.population[r5]))
 
                 # Crossover
-                fixed_idx = np.random.randint(0, len(self.population[0]))
-                fixed_value = self.population[target][fixed_idx]
+                fixed_idx = np.random.randint(0, len(self.population[0])) # Asegurar que al menos un gen viene del trial
+                fixed_value = self.population[trial][fixed_idx]
                 if self.variant_dict["crossover"] == "bin":
                     crossover_mask = np.random.rand(len(self.population[0])) < crossover_rate
                     trial[crossover_mask] = self.population[target][crossover_mask]
                 elif self.variant_dict["crossover"] == "exp":
+                    # Punto de inicio
                     start = np.random.randint(0, len(self.population[0]))
                     j = start
                     while True:
                         trial[j] = self.population[target][j]
+                        # Vuelta al inicio o probabilidad de corte
                         j = (j + 1) % len(self.population[0])
                         if j == start or np.random.rand() > crossover_rate:
                             break
-                trial[fixed_idx] = fixed_value # Valor fijo de target
+                trial[fixed_idx] = fixed_value
                 # Reparar trial
                 trial = np.clip(trial, 0, 1)
                 offspring.append(trial)
                 
             # Mu + Lambda
-            print(f"Evaluando población")
             self.offspring = np.array(offspring)
             self.offspring = np.vstack((self.population, self.offspring))
             
+            # Evaluar la población combinada            
             self.fitness = self.evaluator.evaluate_population(self.offspring)
             self.fitness_history.append(self.fitness)
             fronts = self.ranked_pareto_fronts(self.fitness)
             self.pareto_history.append(fronts)
             self.pareto_fronts = fronts
-            crowding_distances = [self.get_crowding_distance(front, self.fitness) for front in fronts]
 
             # Selección de la nueva población
             new_population = []
-            for i, front in enumerate(fronts):
+            for front in fronts:
                 # Selección de individuos de la misma frente
                 front_individuals = self.offspring[front]
-                front_crowding_distances = crowding_distances[i]
                 # Selecciónar todo el frente, si sobrepasa el tamaño de la población, seleccionar por crowding distance
                 if len(new_population) + len(front) <= n_pop:
                     new_population.extend(front_individuals)
                 else:
                     # Ordenar por crowding distance y seleccionar los mejores
+                    front_crowding_distances = self.get_crowding_distance(front, self.fitness)
                     sorted_indices = np.argsort(front_crowding_distances)[::-1]
                     selected_indices = sorted_indices[:n_pop - len(new_population)]
                     new_population.extend(front_individuals[selected_indices])
             self.population = np.array(new_population)
-
-def main():
-    # Prueba evolución diferencial
-    differential_evolution = DiferentialEvolution()
-    differential_evolution.start(100, 100)
-    for fitness, pareto_fronts in zip(differential_evolution.fitness_history, differential_evolution.pareto_history):
-        print(f"Fitness: {fitness}")
-        print(f"Pareto fronts: {pareto_fronts}")
-        differential_evolution.plot_pareto_fronts(fitness, pareto_fronts)
-        
-    # Imprimir los individuos del frente 1
-    for individual in differential_evolution.pareto_fronts[0]:
-        print(f"Individual: {differential_evolution.population[individual]}")
-    
-    
-if __name__ == "__main__":
-    main()
